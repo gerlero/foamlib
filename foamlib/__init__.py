07d6a50ee436a313b5a15d7c9032730999b63c0e
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-04-15 15:06:30 -0300

    Bump version to 0.3.0

--- foamlib/__init__.py
+++ foamlib/__init__.py
@@ -1,6 +1,6 @@
 """A Python interface for interacting with OpenFOAM."""
 
-__version__ = "0.2.10"
+__version__ = "0.3.0"
 
 from ._cases import AsyncFoamCase, FoamCase, FoamCaseBase
 from ._files import FoamDict, FoamFieldFile, FoamFile
