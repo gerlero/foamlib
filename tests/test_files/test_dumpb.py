90f1feddbd1a712a8bb773984cc92183b3803632
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-04-20 16:02:24 -0300

    Update serialization

---
+++ tests/test_files/test_dumpb.py
@@ -0,0 +1,60 @@
+from foamlib import FoamFile
+from foamlib._files._serialization import dumpb
+
+
+def test_serialize_data() -> None:
+    assert dumpb(1) == b"1"
+    assert dumpb(1.0) == b"1.0"
+    assert dumpb(1.0e-3) == b"0.001"
+    assert dumpb(True) == b"yes"
+    assert dumpb(False) == b"no"
+    assert dumpb("word") == b"word"
+    assert dumpb(("word", "word"), assume_data_entries=True) == b"word word"
+    assert dumpb('"a string"') == b'"a string"'
+    assert dumpb(1, assume_field=True) == b"uniform 1"
+    assert dumpb(1.0, assume_field=True) == b"uniform 1.0"
+    assert dumpb(1.0e-3, assume_field=True) == b"uniform 0.001"
+    assert dumpb([1.0, 2.0, 3.0]) == b"(1.0 2.0 3.0)"
+    assert dumpb([1, 2, 3], assume_field=True) == b"uniform (1 2 3)"
+    assert (
+        dumpb([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], assume_field=True)
+        == b"nonuniform List<scalar> 10(1 2 3 4 5 6 7 8 9 10)"
+    )
+    assert (
+        dumpb([[1, 2, 3], [4, 5, 6]], assume_field=True)
+        == b"nonuniform List<vector> 2((1 2 3) (4 5 6))"
+    )
+    assert (
+        dumpb(FoamFile.DimensionSet(mass=1, length=1, time=-2)) == b"[1 1 -2 0 0 0 0]"
+    )
+    assert (
+        dumpb(
+            FoamFile.Dimensioned(
+                name="g",
+                dimensions=FoamFile.DimensionSet(mass=1, length=1, time=-2),
+                value=9.81,
+            )
+        )
+        == b"g [1 1 -2 0 0 0 0] 9.81"
+    )
+    assert (
+        dumpb(
+            FoamFile.Dimensioned(
+                dimensions=FoamFile.DimensionSet(mass=1, length=1, time=-2), value=9.81
+            )
+        )
+        == b"[1 1 -2 0 0 0 0] 9.81"
+    )
+    assert (
+        dumpb(
+            ("hex", [0, 1, 2, 3, 4, 5, 6, 7], [1, 1, 1], "simpleGrading", [1, 1, 1]),
+            assume_data_entries=True,
+        )
+        == b"hex (0 1 2 3 4 5 6 7) (1 1 1) simpleGrading (1 1 1)"
+    )
+    assert dumpb([{"a": "b"}, {"c": "d"}]) == b"(a b; c d;)"
+    assert (
+        dumpb([{"a": {"b": "c"}}, {"d": {"e": "g"}}])
+        == b"(a\n{\nb c;\n} d\n{\ne g;\n})"
+    )
+    assert dumpb([{"a": [0, 1, 2]}, {"b": {}}]) == b"(a (0 1 2); b\n{\n\n})"
