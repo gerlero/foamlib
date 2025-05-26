73ab1d7b8b3f885cef596b0589ecf602056976c6
Henning Scheufler <henning.scheufler@web.de>
2025-05-26 23:03:58 +0200

    added utility functions to KeyValuePair and FileKey

--- foamlib/preprocessing/_ofDict.py
+++ foamlib/preprocessing/_ofDict.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 from foamlib import FoamFile
 from pathlib import Path
 from pydantic import BaseModel, Field
@@ -10,10 +10,28 @@ class FileKey(BaseModel):
     keys: list[str]
 
     def get_value(self) -> Any:
-        ofDict = FoamFile(self.file_name)
-        return ofDict.get(self.keys[0])
+        of_dict = FoamFile(self.file_name)
+        value = of_dict
+        for key in self.keys:
+            value = value.get(key)
+            if value is None:
+                break
+        return value
 
 
 class KeyValuePair(BaseModel):
-    fileWithKey: FileKey
+    instruction: FileKey
     value: Any
+
+    def set_value(self, case_path: Optional[Path] = None) -> FoamFile:
+        of_file = self.instruction.file_name
+        if case_path is not None:
+            of_file = case_path / of_file
+        if not of_file.exists():
+            raise FileNotFoundError(f"The file {of_file} does not exist.")
+        of_dict = FoamFile(of_file)
+        current_dict = of_dict
+        for key in self.instruction.keys[:-1]:
+            current_dict = current_dict.get(key, {})
+        current_dict[self.instruction.keys[-1]] = self.value
+        return of_dict
