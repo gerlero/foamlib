432a7e5ca05d807426e8d06b764c36a44fe6d4be
Henning Scheufler <henning.scheufler@web.de>
2025-05-27 20:50:26 +0200

    update example

--- example/parametricStudy/createStudy.py
+++ example/parametricStudy/createStudy.py
@@ -5,44 +5,14 @@ from pathlib import Path
 import pandas as pd
 
 from foamlib import FoamCase, FoamFile
+from foamlib.preprocessing.parameter_study import csv_generator
 
 # damBreak
 root = Path(__file__).parent
+template_case = root / "damBreak" 
 
-parastudy = pd.read_csv(root / "parastudy.csv").to_dict(orient="records")
-# %%
-for case in parastudy:
-    case_dir = root / "Cases" / case["case_name"]
-    ofcase = FoamCase(root / "damBreak")
-    ofcase.clone(case_dir)
+study = csv_generator(
+    csv_file=root / "parastudy.csv", template_case=template_case, output_folder=root / "Cases"
+)
 
-    parameter = FoamFile(case_dir / "system" / "simulationsParameters").as_dict()
-    parameter_keys = set(parameter.keys())
-    case_keys = set(case.keys())
-    case_only_keys = case_keys - parameter_keys
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["res1"] = case["res1"]
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["res2"] = case["res2"]
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["res3"] = case["res3"]
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["res4"] = case["res4"]
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["res5"] = case["res5"]
-
-    with FoamFile(case_dir / "system" / "simulationsParameters") as f:
-        f["fluidHeight"] = case["fluidHeight"]
-
-    d = {"parameters": {}}
-    for key in case_only_keys:
-        if key == "case_name":
-            continue
-        d["parameters"][key] = case[key]
-    with open(case_dir / "parameters.json", "w") as f:
-        json.dump(d, f, indent=4)
+study.create_study()
