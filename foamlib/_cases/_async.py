f329d7483de9f89ab37011cb9a8451d922d42d46
Henning Scheufler <henning.scheufler@web.de>
2025-06-28 16:37:57 +0200

    FIX type warnings

--- foamlib/_cases/_async.py
+++ foamlib/_cases/_async.py
@@ -127,7 +127,7 @@ class AsyncFoamCase(FoamCaseRunBase):
     async def _rmtree(
         path: os.PathLike[str] | str, *, ignore_errors: bool = False
     ) -> None:
-        await aioshutil.rmtree(path, ignore_errors=ignore_errors)
+        await aioshutil.rmtree(path, ignore_errors=ignore_errors, onerror=None)
 
     @staticmethod
     async def _copytree(
