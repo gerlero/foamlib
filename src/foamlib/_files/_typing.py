0d1806107913f1a73925775e4976126e6adec5ff
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-12-03 18:02:59 -0300

    Backport typing fix
    Improve typing
    
    (cherry picked from commit 7b31773b1ee2e9c20186829f0bf60c69fd3970ce)

--- src/foamlib/_files/_typing.py
+++ src/foamlib/_files/_typing.py
@@ -26,37 +26,26 @@ TensorLike: TypeAlias = "Tensor | Sequence[float]"
 Field: TypeAlias = "float | np.ndarray[tuple[int] | tuple[int, int], np.dtype[np.float64 | np.float32]]"
 FieldLike: TypeAlias = "Field | TensorLike | Sequence[TensorLike]"
 
-KeywordEntry: TypeAlias = "tuple[DataEntry, DataEntry | Dict]"
-KeywordEntryLike: TypeAlias = "tuple[DataEntryLike, DataEntryLike | DictLike]"
+Dict: TypeAlias = "dict[str, Data | Dict]"
+DictLike: TypeAlias = "Mapping[str, DataLike | DictLike]"
 
-DataEntry: TypeAlias = "str | int | float | bool | Dimensioned | DimensionSet | list[DataEntry | KeywordEntry] | Field"
+KeywordEntry: TypeAlias = tuple["DataEntry", "Data | Dict"]
+KeywordEntryLike: TypeAlias = tuple["DataEntryLike", "Data | DictLike"]
+
+DataEntry: TypeAlias = "str | int | float | bool | Dimensioned | DimensionSet | list[DataEntry | KeywordEntry | Dict] | Field"
 DataEntryLike: TypeAlias = (
-    "DataEntry | Sequence[DataEntryLike | KeywordEntryLike] | FieldLike"
+    "DataEntry | Sequence[DataEntryLike | KeywordEntryLike | DictLike] | FieldLike"
 )
 
 Data: TypeAlias = "DataEntry | tuple[DataEntry, ...]"
 DataLike: TypeAlias = "DataEntryLike | tuple[DataEntryLike, ...]"
 
-StandaloneData: TypeAlias = (
-    "Data"
-    "| np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]"
-    "| np.ndarray[tuple[int, int], np.dtype[np.float64 | np.float32]]"
-    "| list[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]]"
-    "| tuple[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]], np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]]"
-)
-StandaloneDataLike: TypeAlias = (
-    "StandaloneData"
-    "| DataLike"
-    "| Sequence[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]]"
-    "| Sequence[Sequence[int]]"
-    "| tuple[Sequence[int], Sequence[int]]"
-)
+StandaloneData: TypeAlias = "Data | np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]] | np.ndarray[tuple[int, int], np.dtype[np.float64 | np.float32]] | list[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]] | tuple[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]], np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]]"
 
-Dict: TypeAlias = "dict[str, DataEntry | Dict]"
-DictLike: TypeAlias = "Mapping[str, DataEntryLike | DictLike]"
+StandaloneDataLike: TypeAlias = "StandaloneData | DataLike | Sequence[np.ndarray[tuple[int], np.dtype[np.int64 | np.int32]]] | Sequence[Sequence[int]] | tuple[Sequence[int], Sequence[int]]"
 
 SubDict: TypeAlias = "dict[str, Data | SubDict] | MultiDict[str, Data | SubDict]"
-SubDictLike: TypeAlias = DictLike
+SubDictLike: TypeAlias = "Mapping[str, DataLike | SubDictLike]"
 
 File: TypeAlias = "dict[str | None, StandaloneData | Data | SubDict] | MultiDict[str | None, StandaloneData | Data | SubDict]"
 FileLike: TypeAlias = "Mapping[str | None, StandaloneDataLike | DataLike | SubDictLike]"
